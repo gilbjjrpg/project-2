b45e33f2e746395ba9e89ac63340105e5b4558db
gilbjjrpg <gilbjj@farmingdale.edu>
2026-04-22 10:49:09 -0400

    Created loadquestions function to load questions

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -5,3 +5,15 @@ window.addEventListener("DOMContentLoaded", function() {
 function setupQuizPage() {
     
 }
+
+async function loadQuestions() {
+    try {
+        const response = await fetch('../data/questions.json')
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
+
+loadQuestions();
